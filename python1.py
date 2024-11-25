ed481911bd57917983b9d70d5bbc828a5cdb95fd
pathatic-codes <negorov1@ivytech.edu>
2024-11-25 17:52:43 -0500

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,14 @@
+print("Hello world")
+print("I just wrote my first Python Program")
+print("This is so much fun")
+
+#Here is a cooment, As-salamu-alaykum
+
+name = input("YOYOYO, please enter your name here")
+print("Welcome, it is nice to meet you" + name)
+
+c = "cats."
+d = "dogs."
+s = print("It is raining cats and " + d)
+
+
